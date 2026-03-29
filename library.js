1bb44c9897fef2fc18f5ee06ff6e8590671a655c
고예찬 <koyechan1007@gmail.com>
2026-03-29 13:11:26 +0900

    update

--- library.js
+++ library.js
@@ -7,6 +7,7 @@ function onKey(key, callback){
         }
     });
 }
+
 function onKeyUp(key, callback){
     document.addEventListener("keyup", (e) => {
         if(e.key === key){
@@ -14,82 +15,138 @@ function onKeyUp(key, callback){
         }
     });
 }
+
 function createSprite(){
     const sprite = document.createElement("div");
     sprite.style.position = "absolute";
     document.body.appendChild(sprite);
+
     let velocityX = 0;
     let velocityY = 0;
     let rotation = 0;
+    let scaleX = 1;
+    let scaleY = 1;
+    let gravity = 0;
+
     const overlaps = [];
+
     function update(){
-        const x = parseInt(sprite.style.left) || 0;
-        const y = parseInt(sprite.style.top) || 0;
-        sprite.style.left = (x + velocityX) + "px";
-        sprite.style.top = (y + velocityY) + "px";
-        const rectA = sprite.getBoundingClientRect();
+        let x = parseInt(sprite.style.left) || 0;
+        let y = parseInt(sprite.style.top) || 0;
+
+        velocityY += gravity;
+        y += velocityY;
+        sprite.style.top = y + "px";
+
+        let rectA = sprite.getBoundingClientRect();
+
         overlaps.forEach(({target, callback}) => {
             const rectB = target.sprite.getBoundingClientRect();
             if (rectA.left < rectB.right && rectA.right > rectB.left && rectA.top < rectB.bottom && rectA.bottom > rectB.top){
+                callback();
+                if (velocityY > 0){
+                    sprite.style.top = (rectB.top - rectA.height) + "px";
+                    velocityY = 0;
+                } 
+                else if (velocityY < 0){
+                    sprite.style.top = rectB.bottom + "px";
+                    velocityY = 0;
+                }
+            }
+        });
+        y = parseInt(sprite.style.top) || 0;
+        x += velocityX;
+        sprite.style.left = x + "px";
+        rectA = sprite.getBoundingClientRect();
+        overlaps.forEach(({target, callback}) => {
+            const rectB = target.sprite.getBoundingClientRect();
+            if (rectA.left < rectB.right && rectA.right > rectB.left && rectA.top < rectB.bottom && rectA.bottom > rectB.top){
+                if (velocityX > 0){
+                    sprite.style.left = (rectB.left - rectA.width) + "px";
+                    velocityX = 0;
+                } else if (velocityX < 0){
+                    sprite.style.left = rectB.right + "px";
+                    velocityX = 0;
+                }
                 callback();
             }
         });
         requestAnimationFrame(update);
     }
     update();
+
     return {
         sprite,
+
         setVelocityX(value){
             velocityX = value;
         },
+
         setVelocityY(value){
             velocityY = value;
         },
+
+        setGravity(value){
+            gravity = value;
+        },
+
         get velocityX(){
             return velocityX;
         },
+
         get velocityY(){
             return velocityY;
         },
+
         moveX(value){
             const current = parseInt(this.sprite.style.left) || 0;
             this.sprite.style.left = (current + value) + "px";
         },
+
         moveY(value){
             const current = parseInt(this.sprite.style.top) || 0;
             this.sprite.style.top = (current + value) + "px";
         },
+
         setSize(x_len, y_len){
             this.sprite.style.width = x_len + "px";
             this.sprite.style.height = y_len + "px";
         },
+
         setPosition(x_pos, y_pos){
             this.sprite.style.left = x_pos + "px";
             this.sprite.style.top = y_pos + "px";
         },
+
         turn(degree){
             rotation += degree;
-            this.sprite.style.transform = `rotate(${rotation}deg)`;
+            this.sprite.style.transform = `rotate(${rotation}deg) scaleX(${scaleX}) scaleY(${scaleY})`;
         },
+
         hide(){
             this.sprite.style.display = "none";
         },
+
         show(){
             this.sprite.style.display = "block";
         },
+
         onOverlap(target, callback){
             overlaps.push({ target, callback });
         },
+
         onClick(callback){
             this.sprite.addEventListener("mousedown", () => {
                 callback();
             });
         },
+
         onClickUp(callback){
             this.sprite.addEventListener("mouseup", () => {
                 callback();
             });
         },
+
         setAnimation(pictures, ms){
             if (this.animationId) clearInterval(this.animationId);
             let scenes = 0;
@@ -102,8 +159,10 @@ function createSprite(){
             target.onOverlap(this, () => {
                 const x = parseInt(target.sprite.style.left) || 0;
                 const y = parseInt(target.sprite.style.top) || 0;
-                target.sprite.style.left = (x - target.velocityX) + "px";
-                target.sprite.style.top = (y - target.velocityY) + "px";
+                if (target.velocityY > 0){
+                    target.sprite.style.top = (y - target.velocityY) + "px";
+                    target.setVelocityY(0);
+                }
             });
         },
         stopAnimation(){
@@ -112,12 +171,24 @@ function createSprite(){
                 this.animationId = null;
             }
         },
+        flipX(value){
+            scaleX = value ? -1 : 1;
+            this.sprite.style.transform = `rotate(${rotation}deg) scaleX(${scaleX}) scaleY(${scaleY})`;
+        },
+
+        flipY(value){
+            scaleY = value ? -1 : 1;
+            this.sprite.style.transform = `rotate(${rotation}deg) scaleX(${scaleX}) scaleY(${scaleY})`;
+        },
+
         set x(value){
             this.sprite.style.left = value + "px";
         },
+
         set y(value){
             this.sprite.style.top = value + "px";
         },
+
         set texture(value){
             this.sprite.style.backgroundImage = `url(${value})`;
             this.sprite.style.backgroundSize = "cover";
